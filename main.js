86c46fef45b51016c342c0ff43ec9fbc6308cd73
Morgan Schirmer <schirmmr@mail.uc.edu>
2024-02-24 21:15:26 -0500

    Added styling

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ async function changeImage() {
     console.log(image[0]);
     document.getElementById("dog").src = image[0];
 }
+
+async function galaxyMode() {
+    console.log(1)
+    document.getElementById("idBody").style.backgroundImage= "url('https://th.bing.com/th/id/R.c2501d8117269d8ce2c7874563383281?rik=%2fG3t0faBx8x%2fOw&pid=ImgRaw&r=0.gif')";
+}
+
